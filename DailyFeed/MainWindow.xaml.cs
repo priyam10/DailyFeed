f87e85da65ed53bc9d7e71cfe3b0a415ac214558
priyam10 <patelpriyam1011@gmail.com>
2017-07-03 21:33:59 -0400

    Add files via upload
    Final version.

---
+++ DailyFeed/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using DailyFeed.ViewModel;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DailyFeed
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new GeneralViewModel();
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            base.OnDeactivated(e);
+            rect.Visibility = System.Windows.Visibility.Collapsed;
+            MainButton.Content = ">";
+            tabView.Visibility = System.Windows.Visibility.Collapsed;
+            gridCol1.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (rect.Visibility == System.Windows.Visibility.Collapsed)
+            {
+                rect.Visibility = System.Windows.Visibility.Visible;
+                (sender as Button).Content = "<";
+                tabView.Visibility = System.Windows.Visibility.Visible;
+                gridCol1.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                rect.Visibility = System.Windows.Visibility.Collapsed;
+                (sender as Button).Content = ">";
+                tabView.Visibility = System.Windows.Visibility.Collapsed;
+                gridCol1.Visibility = System.Windows.Visibility.Collapsed;
+            }
+        }
+
+
+    }
+}
---
+++ DailyFeed/ViewModel/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using PropertyChanged;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyFeed.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
